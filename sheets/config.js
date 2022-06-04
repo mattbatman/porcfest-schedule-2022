4af2bd9d33989816c4f804e2bc8a4fad2727dec4
Matt Batman <batman.matt@gmail.com>
2022-06-04 19:24:03 -0400

    Fetch schedule data with google sheets

---
+++ sheets/config.js
@@ -0,0 +1,9 @@
+const config = {
+	sheets: {
+		id: '1Xgr9ucp1JSXAG0TozNSWtIqhSTUrmjhDwygCKz4k_Qs',
+    outputPath: 'src/data/schedule.json',
+    range: 'Attendee View!B:E'
+  },
+};
+
+module.exports = config;
