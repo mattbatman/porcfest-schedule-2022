e6663cee39ede25d502f7fa37861d4662c151e8e
Matt Batman <batman.matt@gmail.com>
2022-06-05 19:41:22 -0400

    Display event titles as table by time and location

---
+++ src/components/Block.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { find } from 'ramda';
+
+const Block = ({ data, allLocations }) => {
+	const { date, events } = data;
+	return (
+		<tr>
+			<td>{date}</td>
+			{allLocations.map(d => {
+				const event = find(e => e.Location === d)(events);
+				const title = event && event.Title ? event.Title : '';
+				return (
+					<td>{title}</td>
+				)
+			})}
+		</tr>
+	);
+}
+
+export default Block;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
-import * as React from "react";
+import React from 'react';
+import { findIndex, uniq } from 'ramda';
 import scheduleData from '../data/schedule.json';
+import Block from '../components/Block';
+
+const data = scheduleData.reduce((acc, cv) => {
+      const { Date } = cv;
+      const eventIndex = findIndex(existingGroup => existingGroup.date === Date, acc);
+      
+      if (eventIndex < 0) {
+        const events = [];
+        events.push(cv)
+
+        acc.push({
+          date: Date,
+          events
+        })
+
+        return acc;
+      }
+
+      acc[eventIndex].events.push(cv)
+      
+      return acc;
+}, []);
+    
+const locations = scheduleData.map(d => d.Location);
+const allUniqueLocations = uniq(locations);
 
 const IndexPage = () => {
   return (
@@ -8,6 +34,19 @@ const IndexPage = () => {
       <h1>
         PorcFest Schedule 2022 
       </h1>
+      <table>
+        <tr>
+          <th>Time</th>
+          {
+            allUniqueLocations.map(d => (
+                <th key={d}>{d}</th>
+            ))
+          }
+        </tr>
+        {
+          data.map(d => <Block data={d} key={d.date} allLocations={allUniqueLocations} />)
+        }
+      </table>
     </main>
   )
 }
