2ebaed615d01d6716c014c1bf4a204ac6318dd84
Matt Batman <batman.matt@gmail.com>
2022-06-09 21:08:54 -0400

    Fix duration display

--- src/components/ListRow.js
+++ src/components/ListRow.js
@@ -3,16 +3,19 @@ import React from 'react';
 const ListRow = ({ date, events }) => {
   return (
     <>
-      {events.map(({ Date, Title, Link, Duration, Location }, i) => (
-        <tr key={i}>
-          {i === 0 ? <td rowSpan={events.length}>{Date}</td> : null}
-          <td>
-            <a href={Link}>{Title}</a>
-          </td>
-          <td>{Location}</td>
-          <td>{Duration}</td>
-        </tr>
-      ))}
+      {events.map((event, i) => {
+        const { Date, Title, Link, Location } = event;
+        return (
+          <tr key={i}>
+            {i === 0 ? <td rowSpan={events.length}>{Date}</td> : null}
+            <td>
+              <a href={Link}>{Title}</a>
+            </td>
+            <td>{Location}</td>
+            <td>{event['Duration (Minutes)']}</td>
+          </tr>
+        );
+      })}
     </>
   );
 };
